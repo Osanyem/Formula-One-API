5d3e658bba7d3846d6d082f38bd0540a3ba8c183
Osanyem Osadebe <osanyemo@gmail.com>
2025-03-18 12:27:14 -0400

    Create RaceResult repository class

---
+++ src/main/java/com/osanyemo/f1_api/repository/RaceResultRepository.java
@@ -0,0 +1,7 @@
+package com.osanyemo.f1_api.repository;
+
+import com.osanyemo.f1_api.entity.RaceResult;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RaceResultRepository extends CrudRepository<RaceResult, Long> {
+}
