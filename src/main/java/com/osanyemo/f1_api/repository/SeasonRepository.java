b38d15c18c31b50adab3f004c78819237decb83b
Osanyem Osadebe <osanyemo@gmail.com>
2025-03-18 12:32:00 -0400

    Create Season repository class

---
+++ src/main/java/com/osanyemo/f1_api/repository/SeasonRepository.java
@@ -0,0 +1,7 @@
+package com.osanyemo.f1_api.repository;
+
+import com.osanyemo.f1_api.entity.Season;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SeasonRepository extends CrudRepository<Season, Long> {
+}
