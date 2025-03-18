c422d6c200f7ba0a7bc6e96026b32666d4fd7af5
Osanyem Osadebe <osanyemo@gmail.com>
2025-03-18 12:28:57 -0400

    Create Circuit repository class

---
+++ src/main/java/com/osanyemo/f1_api/repository/CircuitRepository.java
@@ -0,0 +1,7 @@
+package com.osanyemo.f1_api.repository;
+
+import com.osanyemo.f1_api.entity.Circuit;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CircuitRepository extends CrudRepository<Circuit, Long> {
+}
