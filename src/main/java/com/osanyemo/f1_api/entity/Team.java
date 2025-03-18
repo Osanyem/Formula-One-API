be3faeb2cf92eb96896209ca4850da278410177f
Osanyem Osadebe <osanyemo@gmail.com>
2025-03-18 11:21:02 -0400

    Create Team entity

--- src/main/java/com/osanyemo/f1_api/entity/Team.java
+++ src/main/java/com/osanyemo/f1_api/entity/Team.java
@@ -1,4 +1,31 @@
 package com.osanyemo.f1_api.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "teams")
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String nationality;
+
+    private String baseLocation;
+
+    private String teamPrincipal;
+
+    @Column(nullable = false)
+    private String constructor;
 }
